b6728e49bb3d8040678e3d477b4c91ccd81c247a
chorsey <caylahorsey@gmail.com>
2024-02-27 20:51:58 -0500

    Completed test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+
+
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id= "victory";
+
+newHeader.textContent= "Cayla is the champion"
+
